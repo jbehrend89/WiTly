d10778cb2ffd07db5224a8df83b45d72abe137ef
jbehrend89 <84775195+jbehrend89@users.noreply.github.com>
2021-09-04 15:38:37 +0800

    Updates

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { peopleData } from './fixtures/people.js';
 const app = express();
 app.use(express.static('static'));
 app.set('view engine', 'ejs');
-app.set('views', './views/');
+app.set('views', './views/pages/');
 
 app.listen(3000, () => {
     console.log('Server started!');
